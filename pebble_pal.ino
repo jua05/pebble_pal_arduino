584707417f345c2e6d86c1b808dec9e178ee2997
jua05 <sjua1306@gmail.com>
2024-10-08 20:04:22 +0900

    ⚡️ 기능 연결
    led 끄고 키는 방법 2가지로 연결
    - 블루투스
    - 터치센서
    
    블루투스, 충격센서 기능 구현 필요

--- pebble_pal.ino
+++ pebble_pal.ino
@@ -1,14 +1,26 @@
 #include <BluetoothSerial.h>
-BluetoothSerial mybt;
-String ms = "";
-int touch_switch; // 터치 센서
+BluetoothSerial mybt; //블루투스
+String ms = ""; //블루투스 입력 값 변수
+
+#include <Adafruit_NeoPixel.h>
+#define PIN 25 //led
+#define NUMPIXELS 8 //NeoPixel 링 크기
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); //개수, 핀번호 , 픽셀색상순서, 데이터 전송 속도
+
+#define SHOCK 34 //충격센서 핀 번호
+
+#define TOUCH 20 //터치센서 감도
+int touch; // 터치 센서
 unsigned long previousMillis = 0; // 이전 시간 저장 변수
 const long interval = 500; // 터치 센서 체크 간격
 
 void setup() {
   mybt.begin("Pebble_pal"); // 블루투스 이름 설정
   Serial.begin(115200); // 시리얼 통신 
-  pinMode(2, OUTPUT); // LED 핀 설정
+
+  pixels.begin(); // NeoPixel 스트립 객체 초기화 (필수)
+
+  pinMode(SHOCK, INPUT); //쇼크 핀 
 }
 
 void loop() { 
@@ -17,18 +29,44 @@ void loop() {
   // 1초마다 터치 센서 읽기
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis; // 이전 시간을 현재 시간으로 업데이트
-    touch_switch = touchRead(14); // 터치 센서 핀 읽기
-    Serial.println(touch_switch); // 터치 반응 확인
+    touch = touchRead(14); // 터치 센서 핀 읽기
+    if(touch<TOUCH){
+        mybt.println("read Touch"); // 휴대폰으로 블루투스 신호 전송
+        if (ms == "2") {
+        for(int i=0;i<NUMPIXELS;i++){
+          pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+          pixels.show();
+          ms = "1";
+        }
+      } else {
+        pixels.clear(); // 모든 픽셀 색상을 '꺼짐'으로 설정 // LED 끄기
+        pixels.show();
+        ms = "2";
+      }
+    }
+    Serial.println(touch); // 터치 반응 확인
     Serial.println("\n");
   }
+
+  //충격 센서 감지 시
+  if (digitalRead(SHOCK) == HIGH) {
+      Serial.println("SHOCK!!!!");
+      mybt.println("read shock");
+    }
 
   // Bluetooth 메시지 수신 처리
   if (mybt.available()) {
     ms = mybt.readStringUntil('c');
+    Serial.println(ms);
     if (ms == "1") {
-      digitalWrite(2, HIGH); // LED 켜기
+      //digitalWrite(2, HIGH); // LED 켜기
+      for(int i=0;i<NUMPIXELS;i++){
+        pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+        pixels.show();
+      }
     } else {
-      digitalWrite(2, LOW); // LED 끄기
+      pixels.clear(); // 모든 픽셀 색상을 '꺼짐'으로 설정 // LED 끄기
+      pixels.show();
     }
   }   
 }
